7da5c1848e5fdd401c60156caa41e57c04b437c4
William Soukkachang <Wsoukkachang@yahoo.com>
2020-01-07 22:01:42 -0600

    Completed insert def.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,7 +12,16 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value >= self.value:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+        elif value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+        else:
+            self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
